568ee74b67cc5b9ed16458ac455d948443e7fbf6
Jessedekeizer <jessedekeizer03@gmail.com>
2022-05-26 11:12:59 +0200

    packages

---
+++ DigitaalProgrammaBoekje/DigitaalProgrammaBoekje/Pages/Database/DbUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace ProjectWebDev.Pages.Databasestuff
+{
+    public class DbUtils
+    {   
+        public IDbConnection Connect()
+        {
+            string connectionString = "Server=127.0.0.1;Port=3306;Database=Webdev;Uid=root;Pwd=Test@1234!;";
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
