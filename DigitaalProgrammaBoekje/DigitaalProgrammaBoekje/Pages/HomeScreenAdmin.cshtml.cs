11a7a58b865db43197bf420b27971ae5ba9e45fc
Jelle5 <91126597+Jelle5@users.noreply.github.com>
2022-06-01 12:02:41 +0200

    jo

--- DigitaalProgrammaBoekje/DigitaalProgrammaBoekje/Pages/HomeScreenAdmin.cshtml.cs
+++ DigitaalProgrammaBoekje/DigitaalProgrammaBoekje/Pages/HomeScreenAdmin.cshtml.cs
@@ -7,14 +7,64 @@ namespace DigitaalProgrammaBoekje.Pages;
 
 public class HomeScreenAdmin : PageModel
 {
-    public IEnumerable<Festival> Festivallist { get; set; }
-    public void OnGet()    
-    {
-        Festivallist = new FestivalRepository().FestivalsGet();
-    }
+    
+        private string Photo;
+        private IWebHostEnvironment Environment;
+        public IEnumerable<Festival> Festivallist { get; set; }
 
-    public IActionResult OnPostAddFestival()
-    {
-        return Page();
+        [BindProperty] 
+        public Festival Festival { get; set; }
+
+        public HomeScreenAdmin(IWebHostEnvironment _environment)
+        {
+            Environment = _environment;
+        }
+
+        public void OnGet()
+        {
+            FestivalRepository festivallist = new FestivalRepository();
+            Festivallist = festivallist.FestivalsGet();
+        }
+
+        public IActionResult OnPostAddFestival()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPostUpload(List<IFormFile> frontPosted)
+        {
+            string path = Path.Combine(this.Environment.WebRootPath, "content");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            foreach (IFormFile postedFile in frontPosted)
+            {
+                string fileName = null;
+                int i = 0;
+                string extension = Path.GetExtension(postedFile.FileName);
+                while (i == 0)
+                {
+                    fileName = Path.GetRandomFileName();
+                    fileName = Path.ChangeExtension(fileName, extension);
+
+                    if (!System.IO.File.Exists(fileName))
+                    {
+                        i++;
+                        Photo = fileName;
+                    }
+                }
+
+                using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+            }
+
+
+            FestivalRepository festival = new FestivalRepository();
+            festival.AddFestival(Festival.Festival_naam, Festival.Festival_locatie, Festival.Festival_datum, Photo, 1);
+            return RedirectToPage("/HomeScreenAdmin");
+        }
     }
-}
