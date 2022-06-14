ca0cc12e40d99499e14191c3c78834a55f778ee1
Jelle5 <91126597+Jelle5@users.noreply.github.com>
2022-06-14 14:15:20 +0200

    dgf

---
+++ DigitaalProgrammaBoekje/DigitaalProgrammaBoekje/Pages/Bedrijven.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Policy;
+using DigitaalProgrammaBoekje.Pages.Database.Models;
+using DigitaalProgrammaBoekje.Pages.Database.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DigitaalProgrammaBoekje.Pages;
+
+public class Bedrijven : PageModel
+{
+    public IEnumerable<Bedrijf> Bedrijfs { get; set; }
+    
+    public IEnumerable<Jurylid> Jurylids { get; set; }
+    
+    public Jurylid Jurylid { get; set; }
+
+    public void OnGet()
+    {
+        Jurylids = new JurylidRepository().GetAllJury();
+
+        Bedrijfs = new BedrijfRepository().GetAllbedrijf();
+        
+    }
+
+    public IActionResult OnPostJuryUpd([FromForm]int jury_id)
+    {
+        
+        return RedirectToPage("/Update jury", new{Jury_id = jury_id});
+    }
+}
---
+++ DigitaalProgrammaBoekje/DigitaalProgrammaBoekje/Pages/Update jury.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using DigitaalProgrammaBoekje.Pages.Database.Models;
+using DigitaalProgrammaBoekje.Pages.Database.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DigitaalProgrammaBoekje.Pages;
+
+public class Update_jury : PageModel
+{
+    public IEnumerable<Jurylid> Jurylids { get; set; }
+    
+    public void OnGet([FromQuery] int jury_id)
+    {
+        Jurylids = new JurylidRepository().Get1Jury(jury_id);
+    }
+}
