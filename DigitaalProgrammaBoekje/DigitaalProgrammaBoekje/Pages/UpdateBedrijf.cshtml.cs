81fc52a05579f06a83e9e30c9cdb84058f447e53
Jelle5 <91126597+Jelle5@users.noreply.github.com>
2022-06-16 11:24:15 +0200

    Update bedijrf + jury

---
+++ DigitaalProgrammaBoekje/DigitaalProgrammaBoekje/Pages/UpdateBedrijf.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using DigitaalProgrammaBoekje.Pages.Database.Models;
+using DigitaalProgrammaBoekje.Pages.Database.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DigitaalProgrammaBoekje.Pages;
+
+public class UpdateBedrijf : PageModel
+{
+    
+    public IEnumerable<Bedrijf> Bedrijfs { get; set; }
+    
+    public IActionResult OnGet([FromQuery] int bedrijf_id)
+    {
+        Bedrijfs = new BedrijfRepository().Get1Bedrijf(bedrijf_id);
+        return Page();
+    }
+    
+    public IActionResult OnPostUpdateNaam([FromForm] string NaamUpd, [FromForm] int bedrijf_id)
+    {
+        Bedrijfs = new BedrijfRepository().UpdateNaamBedrijf(bedrijf_id, NaamUpd);
+
+        return RedirectToPage(new {Bedrijf_id = bedrijf_id});
+    }
+
+    public IActionResult OnPostUpdateLink([FromForm] string LinkUpd, [FromForm] int bedrijf_id)
+    {
+        Bedrijfs = new BedrijfRepository().UpdateLinkBedrijf(bedrijf_id, LinkUpd);
+
+        return RedirectToPage(new {Bedrijf_id = bedrijf_id});
+    }
+    
+    public IActionResult OnPostTerug()
+    {
+        return RedirectToPage("/Bedrijven");
+    }
+}
